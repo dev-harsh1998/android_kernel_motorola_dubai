66fe15bb6f698c6422207723b13e63c56e8c0c0c
Vlad Karpovich <Vlad.Karpovich@cirrus.com>
2020-07-02 19:10:05 -0500

    ASoC: cs35l41: Add Android build file
    It uses AndroidKernelModule.mk to build kernel modules
    as part of Android build.
    
    Signed-off-by: Vlad Karpovich <Vlad.Karpovich@cirrus.com>
    Change-Id: Ia07824f1d9f6110c2de4c5fd0a657ea729efd6c2

---
+++ sound/soc/codecs/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cirrus_wm_adsp.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(TOP)/vendor/qcom/opensource/core-utils/dlkm/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+ifeq ($(CIRRUS_AMP_CODEC),cs35l41_spi)
+KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_SPI=y
+else
+KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_I2C=y
+endif
+KERNEL_CFLAGS := -I$(MODULES_TOP)/include
+LOCAL_MODULE := cirrus_cs35l41.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(TOP)/vendor/qcom/opensource/core-utils/dlkm/AndroidKernelModule.mk
+
