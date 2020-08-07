26145bca28d118a58b61cb8e8b016769f18241db
huangys <huangys@motorola.com>
2020-08-07 15:47:19 +0800

    cs35l41: Fix build config on QC SM8350
    - the default config is spi bus, if you need use i2c bus driver.
      define the variable in product config.
    - fix building the head file can't be searched
    
    Change-Id: Ic260328f464de7f2783e1593655620448094b3ca
    Signed-off-by: huangys <huangys@motorola.com>

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -1,10 +1,12 @@
+DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := cirrus_wm_adsp.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(TOP)/vendor/qcom/opensource/core-utils/dlkm/AndroidKernelModule.mk
+include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 ifeq ($(CIRRUS_AMP_CODEC),cs35l41_spi)
@@ -15,5 +17,5 @@ endif
 LOCAL_MODULE := cirrus_cs35l41.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(TOP)/vendor/qcom/opensource/core-utils/dlkm/AndroidKernelModule.mk
+include $(DLKM_DIR)/AndroidKernelModule.mk
 
