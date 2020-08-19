1b30f6fbbbc3cf63df78e026589b7d79bfcd5964
huangys <huangys@motorola.com>
2020-08-19 21:34:24 +0800

    ASoc: cs35l41: set SPI config as default
    if we define nothing in product config about CIRRUS_AMP_CODEC
    variable. spi bus driver will be built on default. if you'd like
    i2c driver to instead, you must config in product folder.
    
    Change-Id: I5f8155041c600c45f38e1d5ef1ccc76e6e4a2c4e
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1727570
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -2,10 +2,10 @@ DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-ifeq ($(CIRRUS_AMP_CODEC),cs35l41_spi)
-KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_SPI=y
-else
+ifeq ($(CIRRUS_AMP_CODEC),cs35l41_i2c)
 KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_I2C=y
+else
+KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_SPI=y
 endif
 LOCAL_MODULE := cirrus_cs35l41.ko
 LOCAL_MODULE_TAGS := optional
